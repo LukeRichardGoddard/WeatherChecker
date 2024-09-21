7d0314b1fae587e7fba725da2ab41f57156b8133
Luke Richard Goddard <github@lukerichardgoddard.com>
2024-09-21 15:43:47 +1000

    Initial Commit

---
+++ WeatherChecker/WeatherCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCheckerApp.swift
+//  WeatherChecker
+//
+//  Created by Luke Goddard on 21/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
